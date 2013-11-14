0e61d919d93cc85b835ea224a8d10ad5e9d9c973
trlivingston <trlivingston@paypal.com>
2013-11-14 16:41:42 -0600

    Using multilevel + leveldb

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 'use strict';
 
-var pkg = require('./package.json'),
+var cluster = require('cluster'),
+    server = require('./lib/server').create(),
+    levelstats = require('./lib/levelstats'),
+    pkg = require('./package.json'),
     name = pkg.name,
-    version = pkg.version,
-    stats = require('./lib/stats');
+    version = pkg.version;
 
 exports = module.exports = {
 
@@ -12,8 +14,17 @@ exports = module.exports = {
     version: version,
 
     register: function (plugin, options, next) {
-        var hapi = plugin.hapi,
-            settings = hapi.utils.applyToDefaults(require('./config/settings.json'), options);
+        var hapi, settings, stats;
+
+        hapi = plugin.hapi;
+
+        settings = hapi.utils.applyToDefaults(require('./config/settings.json'), options);
+
+        if (cluster.isMaster) {
+            server.listen(settings.levelport || 3000);
+        }
+
+        stats = levelstats(settings);
 
         plugin.route({
             method: 'GET',
@@ -22,21 +33,19 @@ exports = module.exports = {
             handler: stats.handler
         });
 
-
         plugin.ext('onRequest', function (req, next) {
-            stats.increment('requests:total');
-            stats.increment('requests:active');
+            stats.increment('requests.total');
+            stats.increment('requests.active');
             next();
         });
 
         plugin.ext('onPreResponse', function (req, next) {
             var res = req.response();
-            stats.decrement('requests:active');
+            stats.decrement('requests.active');
             if (res._code >= 500) {
-                stats.increment('response:errors');
+                stats.increment('response.errors');
             }
             next();
         });
-
     }
 };
